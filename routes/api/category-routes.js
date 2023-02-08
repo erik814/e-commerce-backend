ec0e34468abe6504bec9f9063bf8c592a9553377
erik814 <erikneafus@gmail.com>
2023-02-07 18:50:42 -0600

    Category routes are done.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,10 +29,31 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then((newCat) => {
+      res.json(newCat);
+    })
+    .catch((err) =>{
+      res.json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      }
+    }
+  )
+    .then((updatedCat) => {
+      res.json(updatedCat);
+    })
+    .catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
